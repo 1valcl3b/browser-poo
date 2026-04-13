f1928ae4a1191dee08c41521b24f1c6d325ac035
ivalcleb <ivalclebbenigno07@gmail.com>
2026-04-13 17:22:39 -0300

    Mudanças

--- browser.py
+++ browser.py
@@ -1,41 +1,47 @@
 class Historico:
     def __init__(self):
         self.pilha = []
-
-    def empilhar(self, url: str):
+        
+    def empilhar(self, url):
         self.pilha.append(url)
 
     def desempilhar(self):
-        if not self.esta_vazio():
+        if self.pilha:
             return self.pilha.pop()
         return None
 
-    def esta_vazio(self) -> bool:
-        return len(self.pilha) == 0
+    def listar(self):
+        if not self.pilha:
+           return "[ ]"
 
-    def listar(self) -> str:
-        if self.pilha:
-            return "[" + "][".join(self.pilha) + "]"
-        return "[ ]"
-
+        res = ""
+        for item in self.pilha:
+            res += f"[{item}]"
+        return res
+    
 class Browser:
     def __init__(self):
         self.historico = Historico()
         self.home = None
-        self.urls_validas = self.carregar_arquivo()
+        self.urls_validas = self.carregar_arq()
+        self.historico_completo = []
 
-    def carregar_arquivo(self) -> set:
+    def carregar_arq(self):
         try:
             with open("urls.txt", "r") as f:
-                return {linha.strip() for linha in f if linha.strip()}
+                urls = set()
+                for linha in f:
+                    urls.add(linha.strip())
+            return urls
+
         except FileNotFoundError:
-            print("Arquivo 'urls.txt' não foi encontrado. Iniciando com Historico vazio .")
+            print("Arquivo não foi encontrado. Iniciando com Historico vazio.")
             return set()
 
-    def validar_url(self, url: str) -> bool:
+    def validar_url(self, url) -> bool:
         return url in self.urls_validas
 
-    def adicionar_url(self, url: str):
+    def adicionar_url(self, url):
         if url in self.urls_validas:
             print(f"ERRO: URL '{url}' já existe!")
             return
@@ -51,43 +57,59 @@ class Browser:
         print(f"Home: [{self.home if self.home else ' '}]")
         print("Digite a url ou #back para retornar à última página visitada.")
 
-    def navegar_para(self, url_destino: str):
-        if not self.validar_url(url_destino):
+    def navegar_para(self, url_destino):
+        if self.validar_url(url_destino) == False:
             print(f"ERRO: '{url_destino}' não é uma URL válida!")
             return
+        
         if self.home is not None:
             self.historico.empilhar(self.home)
         self.home = url_destino
 
+        self.historico_completo.append(url_destino)
+
     def voltar(self):
-        if self.historico.esta_vazio():
+        if  not self.historico.pilha:
             print("ERRO: Não há página anterior para voltar!")
             return
+        
         self.home = self.historico.desempilhar()
+
+        self.historico_completo.append(self.home)
         print(f"\nVoltando para {self.home}\n")
 
-    def mostrar_historico(self):
-        print(f"\nHistórico completo: {self.historico.listar()}\n")
+
+    def show_historico(self):
+       if self.historico_completo:
+          historico = "[" + "][".join(self.historico_completo) + "]"
+       else:
+          self.historico = "[ ]"
+       
+       print(f"Historico completo: {historico}")
+          
 
 navegador = Browser()
+
 while True:
   navegador.exibir_tela()
   comando = input("url: ").strip()
+
   if comando == "#sair":
     break
+  
   elif comando == "#back":
     navegador.voltar()
+    
   elif comando == "#showhist":
-    navegador.mostrar_historico()
+    navegador.show_historico()
+    
   elif comando.startswith("#add"):
-    partes = comando.split(maxsplit=1)
-    if len(partes) == 2:
-      url = partes[1]
-      if url.startswith("www"):
-        navegador.adicionar_url(url)
+      partes = comando.split(maxsplit=1)
+      if len(partes) != 2:
+          print("ERRO: use #add <url> ")
       else:
-        print("ERRO: URL deve começar com 'www'")
-    else:
-      print("ERRO: Use #add <url>")
+          url = partes[1]
+          navegador.adicionar_url(url)     
+  
   else:
     navegador.navegar_para(comando)
